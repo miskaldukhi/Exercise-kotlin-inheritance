59f3d0c846881cf51c91e1f3ec8d83b80211c281
miskaldukhi <miskaldukhi@gmail.com>
2025-02-17 18:46:53 +0300

    main created

--- src/Library/Main.kt
+++ src/Library/Main.kt
@@ -1,2 +1,12 @@
 package Library
 
+fun main(){
+    val fiction= FictionBook("The Lord of the Rings","J.R.R. Tolkien",
+        "Fantasy", 56467837)
+
+    val nonFiction = Nonfictional("Sapiens", "Yuval Noah Harari",
+        "History", 788276)
+
+   println(fiction)
+    println(nonFiction)
+}
