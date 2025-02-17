7c50fb6c76d6d142f9c741dde047a078f47f8f6f
miskaldukhi <miskaldukhi@gmail.com>
2025-02-17 18:32:23 +0300

    non fictional done

--- src/Library/nonfictional.kt
+++ src/Library/nonfictional.kt
@@ -1,2 +1,16 @@
 package Library
 
+open class Nonfictional (val title: String,
+                         val author: String,val genre:String, val isbn: Int ) {
+    open fun displayInfo(){
+        println("Title:$title")
+        println("Author: $author")
+        println("ISBN: $isbn")
+        println("Genre:$genre")
+    }
+    fun main(){
+        val nonFictional = Nonfictional("The Autobiography of Malcolm X",
+            "Alex Haley and Malcolm X", "Autobiography", 4658687 )
+        nonFictional.displayInfo()
+    }
+}
