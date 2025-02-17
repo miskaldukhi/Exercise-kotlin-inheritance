709e4993ba485723335059bcd1d2276fbda57888
miskaldukhi <miskaldukhi@gmail.com>
2025-02-17 18:31:44 +0300

    fictional done

--- src/Library/fictional.kt
+++ src/Library/fictional.kt
@@ -1,4 +1,15 @@
 package Library
 
-class fictional {
+open class FictionBook(val title: String,val author: String, val genre:String, val isbn: Int ) {
+    open fun displayInfo(){
+        println("Title:$title")
+        println("Author: $author")
+        println("Genre:$genre")
+        println("ISBN: $isbn")
+    }
+    fun main(){
+        val fictional = FictionBook("The Outsiders","S.E.Hinton","Young Adult",  64673497 )
+        fictional.displayInfo()
+    }
+}
 }
